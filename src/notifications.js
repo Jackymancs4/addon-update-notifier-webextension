c9fba29a71e7e634ba9c8ed562e2a33efcfaf181
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-02 16:12:04 +0100

    Add addon icon to the notification

--- src/notifications.js
+++ src/notifications.js
@@ -1,7 +1,7 @@
 var iconSize = 48 * devicePixelRatio;
 var notificationSize = 80 * devicePixelRatio;
 var buttonSize = 16 * devicePixelRatio;
-var extensionHomepages = {}
+var addonDB = {}
 
 // Helper function which returns a basic notification options object.
 function getNotificationOptions(extension) {
@@ -9,27 +9,33 @@ function getNotificationOptions(extension) {
   return {
     type: 'basic',
     priority: 2,
-    iconUrl: extension.icons ? extension.icons[extension.icons.length-1].url : "chrome://browser/content/extension.svg"
+    iconUrl: extension.icons ? extension.hostPermissions[extension.hostPermissions.length-1].slice(0, -1)+extension.icons[extension.icons.length - 1].url : "chrome://browser/content/extension.svg"
   };
 }
 
 // Helper function which displays a notification.
 function showNotification(notificationId, options) {
 
-    chrome.notifications.create(notificationId, options, function(){
-      chrome.storage.sync.get({autoCloseNotification: DEFAULT_OPTIONS.AUTO_CLOSE_NOTIFICATION}, function(results) {
-        if (results.autoCloseNotification) {
-          // Raises an alarm to close notification after 10s.
-          chrome.alarms.create(notificationId, { when: Date.now() + 1e3 });
-        }
-      });
+  console.log(  options.iconUrl );
+
+  chrome.notifications.create(notificationId, options, function () {
+    chrome.storage.sync.get({
+      autoCloseNotification: DEFAULT_OPTIONS.AUTO_CLOSE_NOTIFICATION
+    }, function (results) {
+      if (results.autoCloseNotification) {
+        // Raises an alarm to close notification after 10s.
+        chrome.alarms.create("close_" + notificationId, {
+          when: Date.now() + 1e3
+        });
+      }
     });
+  });
 }
 
 // Helper function to create notification Id
 function getNotificationId(extension) {
   let id = extension.id + extension.version
-  extensionHomepages[id] = extension.homepageUrl
+  addonDB[id] = extension.id
 
   return id;
 }
@@ -38,7 +44,7 @@ function getNotificationId(extension) {
 function setExtensionUpdateNotificationOptions(extension, oldVersion) {
   var options = getNotificationOptions(extension);
   options.title = chrome.i18n.getMessage('updatedExtensionTitle', [extension.name]),
-  options.message = chrome.i18n.getMessage('updatedExtensionMessage',
+    options.message = chrome.i18n.getMessage('updatedExtensionMessage',
       [extension.name, oldVersion, extension.version])
 
   return options;
@@ -46,13 +52,11 @@ function setExtensionUpdateNotificationOptions(extension, oldVersion) {
 
 function showExtensionUpdateNotification(extension, oldVersion) {
 
-  var gettingInfo = browser.management.get(extension.id);
-  gettingInfo.then(function (info) {
-
-    var options = setExtensionUpdateNotificationOptions(info, oldVersion);
-    showNotification(getNotificationId(info), options);
-
+  addonDB["installed_" + getNotificationId(extension)] = oldVersion
+  chrome.alarms.create("installed_" + getNotificationId(extension), {
+    when: Date.now() + 1e2
   });
+
 }
 
 // Clear notification if user clicks on it.
@@ -60,12 +64,21 @@ function onNotificationsClicked(notificationId) {
 
   console.log(notificationId);
 
-  chrome.storage.sync.get({openHomepage: DEFAULT_OPTIONS.OPEN_HOMEPAGE}, function(results) {
+  chrome.storage.sync.get({
+    openHomepage: DEFAULT_OPTIONS.OPEN_HOMEPAGE
+  }, function (results) {
     if (results.openHomepage) {
-      var creating = browser.tabs.create({
-        url: extensionHomepages[notificationId]
+
+      var gettingInfo = browser.management.get(addonDB[notificationId])
+      gettingInfo.then(function (info) {
+
+        var creating = browser.tabs.create({
+          url: info.homepageUrl
         });
-      }
+
+      });
+
+    }
   })
 
   // Open new options page.
@@ -74,14 +87,14 @@ function onNotificationsClicked(notificationId) {
   }
   var clickedNotification = {};
   clickedNotification[notificationId] = 'clickedByUser';
-  chrome.storage.local.set(clickedNotification, function() {
+  chrome.storage.local.set(clickedNotification, function () {
     chrome.notifications.clear(notificationId);
   });
 }
 
 // Warn the others that this notification has been closed by the user.
 function onNotificationsClosed(notificationId, closedByUser) {
-  chrome.storage.local.get(notificationId, function(results) {
+  chrome.storage.local.get(notificationId, function (results) {
     if (closedByUser || results[notificationId] === 'clickedByUser') {
       var closedNotification = {};
       closedNotification[notificationId] = 'closedByUser';
@@ -99,8 +112,8 @@ function onStorageChanged(changes, area) {
 }
 
 function closeExtensionNotifications(extensionId) {
- chrome.notifications.getAll(function(notificationIds) {
-    Object.keys(notificationIds).forEach(function(notificationId) {
+  chrome.notifications.getAll(function (notificationIds) {
+    Object.keys(notificationIds).forEach(function (notificationId) {
       if (notificationId.indexOf(extensionId) === 0) {
         chrome.notifications.clear(notificationId);
       }
@@ -109,33 +122,29 @@ function closeExtensionNotifications(extensionId) {
 }
 
 function onAlarm(alarm) {
-  // Show new options notification when alarm is received.
-  if (alarm.name === 'newOptions') {
-    chrome.storage.sync.get('newOptions', function(results) {
-      if (results['newOptions'] !== 'closedByUser') {
 
-        var gettingInfo = browser.management.get(chrome.runtime.id);
-        gettingInfo.then(function (info) {
+  if (alarm.name.slice(0, "close_".length) == "close_") {
+      chrome.storage.sync.get({
+        autoCloseNotification: DEFAULT_OPTIONS.AUTO_CLOSE_NOTIFICATION
+      }, function (results) {
+        if (results.autoCloseNotification) {
+          var notificationId = alarm.name.slice("close_".length)
+          chrome.notifications.clear(notificationId);
+        }
+      });
+    } else if (alarm.name.slice(0, "installed_".length) == "installed_") {
 
-          var options = getNotificationOptions(info);
-          options.title = chrome.i18n.getMessage('newOptionsTitle');
-          options.message = chrome.i18n.getMessage('newOptionsText');
+      var gettingInfo = browser.management.get(addonDB[alarm.name.slice("installed_".length)]);
+      gettingInfo.then(function (info) {
 
-          showNotification('newOptions', options);
+        console.log(info)
 
-        });
+        var options = setExtensionUpdateNotificationOptions(info, addonDB[alarm.name]);
+        showNotification(getNotificationId(info), options);
 
-      }
-    });
-  // Clear notification otherwise.
-  } else {
-   chrome.storage.sync.get({autoCloseNotification: DEFAULT_OPTIONS.AUTO_CLOSE_NOTIFICATION}, function(results) {
-     if (results.autoCloseNotification) {
-       var notificationId = alarm.name;
-       chrome.notifications.clear(notificationId);
-     }
-   });
-  }
+      });
+    }
+
 }
 
 function onInstalled(details) {
@@ -153,9 +162,6 @@ function onInstalled(details) {
 
     });
   }
-
-  // Wait for 30 seconds before prompting user about new options.
-  chrome.alarms.create('newOptions', { when: Date.now() + 30e3 });
 }
 
 // Register all listeners.
