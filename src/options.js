0691137af4e852782e144672032d630eff33f6af
Giacomo Rossetto <jackyman_cs4@live.it>
2019-03-15 12:29:05 +0100

    Refactor option page
    Add new option for opening AMO page

--- src/options.js
+++ src/options.js
@@ -4,7 +4,8 @@ var openHomepageCheckbox = document.querySelector('#openHomepage');
 // Saves synced options.
 function saveOptions() {
   var autoCloseNotification = autoCloseNotificationCheckbox.checked;
-  var openHomepage = openHomepageCheckbox.checked
+  var openHomepage = openHomepageCheckbox.value
+
   chrome.storage.sync.set({
     autoCloseNotification: autoCloseNotification,
     openHomepage: openHomepage
@@ -23,9 +24,12 @@ window.onload = function() {
     autoCloseNotification: DEFAULT_OPTIONS.AUTO_CLOSE_NOTIFICATION,
     openHomepage: DEFAULT_OPTIONS.OPEN_HOMEPAGE,
   };
+
   chrome.storage.sync.get(defaultOptions, function(results) {
     autoCloseNotificationCheckbox.checked = results.autoCloseNotification;
-    openHomepageCheckbox.checked = results.openHomepage;
+    openHomepageCheckbox.value = results.openHomepage === true ?
+                                  DEFAULT_OPTIONS.OPEN_HOMEPAGE :
+                                  results.openHomepage;
   });
   autoCloseNotificationCheckbox.onchange = saveOptions;
   openHomepageCheckbox.onchange = saveOptions;
